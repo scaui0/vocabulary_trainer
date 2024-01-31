09d5654fbcdb4b55686ff0b9d319c8aa7c52092a
scaui0 <157880743+scaui0@users.noreply.github.com>
2024-01-31 15:52:39 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,37 @@
+import json
+
+# path = Path(__file__).parent / input("Path: ")
+path = "test.json"
+while True:
+    names = []
+    while True:
+        if name := input("Name: "):
+            names.append(name)
+        else:
+            break
+
+    translations = []
+    while True:
+        if name := input("Translation: "):
+            translations.append(name)
+        else:
+            break
+
+    source_example = input("Source example: ")
+    target_example = input("Translation example: ")
+
+    with open(path, "r+") as file:
+        current_data = json.load(file)
+        current_data["entries"].append({
+            "word": names,
+            "translation": translations,
+            "example": {
+                "source": source_example,
+                "target": target_example
+            }
+        })
+        print(current_data)
+        file.seek(0)
+        json.dump(current_data, file, indent=2)
+        # file.append(json.dumps(current_data))
+        # json.dump(current_data, file)
